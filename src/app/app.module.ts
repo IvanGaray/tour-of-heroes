7e528183845f5c8b9f0f9c67c75d5cf5520b9831
Ivan <divangaray@gmail.com>
2022-01-20 16:17:51 -0300

    [Add]style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
@@ -27,6 +31,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+        MatInputModule,
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,54 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  {
+    id: 11,
+    name: 'Arthas',
+    img: 'https://static.heroesofthestorm.com/images/hero-select/card-portraits/arthas-8664159a5e.jpg',
+  },
+  {
+    id: 12,
+    name: 'Illidan',
+    img: 'https://static.heroesofthestorm.com/images/hero-select/card-portraits/illidan-44e4e09a1c.jpg',
+  },
+  {
+    id: 13,
+    name: 'Uther',
+    img: 'https://static.heroesofthestorm.com/images/hero-select/card-portraits/uther-7a696bccc7.jpg',
+  },
+  {
+    id: 14,
+    name: 'Sylvanas',
+    img: 'https://static.heroesofthestorm.com/images/hero-select/card-portraits/sylvanas-9a0cf3b84f.jpg',
+  },
+  {
+    id: 15,
+    name: 'Mal´Ganis',
+    img: 'https://static.heroesofthestorm.com/images/hero-select/card-portraits/malganis-a1ac224160.jpg',
+  },
+  {
+    id: 16,
+    name: 'Jaina',
+    img: 'https://static.heroesofthestorm.com/images/hero-select/card-portraits/jaina-7bc66f174a.jpg',
+  },
+  {
+    id: 17,
+    name: 'Malfurion',
+    img: 'https://static.heroesofthestorm.com/images/hero-select/card-portraits/malfurion-67364cde03.jpg',
+  },
+  {
+    id: 18,
+    name: 'Gul´dan',
+    img: 'https://static.heroesofthestorm.com/images/hero-select/card-portraits/guldan-07fc0ababf.jpg',
+  },
+  {
+    id: 19,
+    name: 'Magma',
+    img: 'https://static.heroesofthestorm.com/images/hero-select/card-portraits/muradin-d1003a8b30.jpg',
+  },
+  {
+    id: 20,
+    name: 'Kel´Thuzad',
+    img: 'https://static.heroesofthestorm.com/images/hero-select/card-portraits/kelthuzad-e536b7c968.jpg',
+  },
 ];
